1b9d3a052c3bfdd772ca3a30ae6cccec0054c508
LaMonte <austinlamonte@yahoo.com>
2016-03-17 13:39:03 -0500

    new

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,11 @@
 //Use to instantiate app, connect factory & controllers and configure app.
 
-var app = angular.module('', []);
+var app = angular.module('myBlogApp', ['MyBlogApp.controllers']);
 
-app.config([]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/index.html',
+        controller: 'controllers.js'
+    })
+}]);
